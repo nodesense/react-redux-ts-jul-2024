02f1ba2a5a0555e93b9397c2640b41b4bf4a2b09
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2024-08-01 15:34:46 +0530

    route start

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,6 +8,25 @@ import Products from './cart/pages/Products';
 import ReduxCounter from './containers/ReduxCounter';
 import ReduxProducts from './cart/pages/ReduxProducts';
 
+import {BrowserRouter as Router,
+        Switch,
+        Route,
+        Link,
+        NavLink
+} from 'react-router-dom';
+
+const Home = () => (
+    <div>
+        <h2>Home page</h2>
+    </div>
+)
+
+const NotFound = () => (
+    <div>
+        <h2>Page not found</h2>
+    </div>
+)
+
 function App () {
     console.log("App render")
 
@@ -15,34 +34,58 @@ function App () {
     const [lang, setLang] = useState(English)
     // below code create virtual dom whenever function called
     return (
+        <Router>
         <div>
             <LanguageContext.Provider value={lang}>
                 <h2>React app</h2>
-                
-                <button onClick={ () => setFlag(!flag) }>
-                    { flag? "Hide": "Show"  }
-                </button>
-
-                {flag && <ReduxProducts /> }
-
-                {flag && <Products /> }
-
-                <Checkout />
-
                 <button onClick= { () => setLang(English)}>En</button>
                 <button onClick= { () => setLang(Tamil)}>Ta</button>
+                <hr />
                 
-                {flag && <ReduxCounter />}
+                <Switch>
+                {/* exact is mean exact match, by default it does starts with match */}
+                <Route path="/" exact>
+                    <Home />
+                </Route>
 
-                {flag && <Counter />}
+                <Route path="/products">
+                    <Products />
+                </Route>
+
+                <Route path="/redux-products">
+                    <ReduxProducts />
+                </Route>
+
+                <Route path="/checkout">
+                    <Checkout />
+                </Route>
+
+
+                <Route path="/counter">
+                    <Counter />
+                </Route>
                 
-                {flag && <Cart /> }
+                
+                <Route path="/redux-counter">
+                    <ReduxCounter />
+                </Route>
 
-                {/* <Counter /> */}
+                <Route path="/cart">
+                    <Cart />
+                </Route>
+
+                {/* * means match all path ie wildcard */}
+                <Route path="*">
+                    <NotFound />
+                </Route>
+
+                </Switch>
+                  
             </LanguageContext.Provider>
 
             
         </div>
+        </Router>
     )
 
 }
