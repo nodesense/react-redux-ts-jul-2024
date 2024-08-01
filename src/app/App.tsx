c0592d8529a4758ddd50337fc1eabe746d31647f
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2024-08-01 16:10:19 +0530

    route start

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,12 @@
-import React, {useState} from 'react';
+import React, {ReactNode, Suspense, lazy, useState} from 'react';
 import Counter from './components/Counter';
-import Cart from './cart/pages/Cart';
+
+// must not be imported if the module is lazy loaded
+// Cart, cartitem, cartlist component shall be loaded when we click the cart link
+// import Cart from './cart/pages/Cart';
+
 import { LanguageContext, Tamil, English} from './contexts/Language';
+//packad as bundle.js 
 import Checkout from './cart/pages/Checkout';
 import Products from './cart/pages/Products';
 
@@ -15,6 +20,11 @@ import {BrowserRouter as Router,
         NavLink
 } from 'react-router-dom';
 
+// browser feature called import() func, not import state
+// webpack shalll pack Cart and its sub-componnents in to separate bundle
+//Cart code shall not be part of bundle.js
+const Cart = lazy ( () => import ("./cart/pages/Cart") )
+
 const Home = () => (
     <div>
         <h2>Home page</h2>
@@ -27,6 +37,34 @@ const NotFound = () => (
     </div>
 )
 
+// place-holder to show when the lazy module is downloading...
+const Loading = () => (
+    <div><h2>loading module...</h2></div>
+)
+ 
+
+interface HeaderProps {
+    children: ReactNode
+}
+// <Header> <h1>React</h1> </Header ==> <h1>React</h1> is known as children
+// children is special property in react, react keyword
+const Header = (props: HeaderProps) => {
+    return (
+        <div>
+           {props.children}
+
+            <NavLink to="/" >Home</NavLink>
+            <NavLink to="/products" >Products</NavLink>
+            <NavLink to="/redux-products" >Redux Products</NavLink>
+            <NavLink to="/cart" >Cart</NavLink>
+            <NavLink to="/redux-counter" >Redux Counter</NavLink>
+            <NavLink to="/counter" >Counter</NavLink>
+
+            
+        </div>
+    )
+}
+
 function App () {
     console.log("App render")
 
@@ -37,7 +75,11 @@ function App () {
         <Router>
         <div>
             <LanguageContext.Provider value={lang}>
-                <h2>React app</h2>
+                <Header >
+                    {/*passed as props.children */}
+                    <h2>React app</h2>
+                </Header>
+                
                 <button onClick= { () => setLang(English)}>En</button>
                 <button onClick= { () => setLang(Tamil)}>Ta</button>
                 <hr />
@@ -71,7 +113,9 @@ function App () {
                 </Route>
 
                 <Route path="/cart">
-                    <Cart />
+                    <Suspense fallback={<Loading />} >
+                        <Cart />
+                    </Suspense>
                 </Route>
 
                 {/* * means match all path ie wildcard */}
